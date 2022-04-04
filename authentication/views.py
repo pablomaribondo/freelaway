6b60a4dcfb380b0bcd49ec4df01be56756d12253
Pablo Maribondo <pablomgvieira@gmail.com>
2022-04-04 20:57:27 -0300

    feat: add validations to register form

--- authentication/views.py
+++ authentication/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 
 def register(request):
+    register_url = '/auth/register'
+
     if request.method == "GET":
         return render(request, 'register.html')
     elif request.method == "POST":
@@ -10,6 +12,11 @@ def register(request):
         password = request.POST.get('password')
         password_confirmation = request.POST.get('password-confirmation')
 
+        if password != password_confirmation:
+            return redirect(register_url)
+
+        if len(username.strip()) == 0 or len(password.strip()) == 0:
+            return redirect(register_url)
 
 def login(request):
     return HttpResponse('Login')
