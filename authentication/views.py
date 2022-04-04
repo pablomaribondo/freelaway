5700e408c134df37c17a582572b0eebbafeafee5
Pablo Maribondo <pablomgvieira@gmail.com>
2022-04-04 20:54:00 -0300

    feat: diferentiate HTTP methods for register endpoint

--- authentication/views.py
+++ authentication/views.py
@@ -3,7 +3,12 @@ from django.http import HttpResponse
 
 
 def register(request):
-    return render(request, 'register.html')
+    if request.method == "GET":
+        return render(request, 'register.html')
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        password_confirmation = request.POST.get('password-confirmation')
 
 
 def login(request):
