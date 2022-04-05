4acd99764613834a940c684c20cce393923fbe21
Pablo Maribondo <pablomgvieira@gmail.com>
2022-04-04 21:08:33 -0300

    feat: add messages

--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.messages import constants
 
 
 def register(request):
@@ -14,23 +16,33 @@ def register(request):
         password_confirmation = request.POST.get('password-confirmation')
 
         if password != password_confirmation:
+            messages.add_message(request, constants.ERROR,
+                                 'As senhas não coincidem')
             return redirect(register_url)
 
         if len(username.strip()) == 0 or len(password.strip()) == 0:
+            messages.add_message(request, constants.ERROR,
+                                 'Preencha todos os campos')
             return redirect(register_url)
 
         user = User.objects.filter(username=username)
 
         if user.exists():
+            messages.add_message(request, constants.ERROR,
+                                 'Esse usuário já existe')
             return redirect(register_url)
 
         try:
             user = User.objects.create_user(
                 username=username, password=password)
             user.save()
+            messages.add_message(request, constants.SUCCESS,
+                                 'Usuário criado com sucesso')
 
             return redirect('/auth/login')
         except:
+            messages.add_message(request, constants.ERROR,
+                                 'Erro interno do sistema')
             return redirect(register_url)
 
 
--- freelaway/settings.py
+++ freelaway/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from django.contrib.messages import constants
 from pathlib import Path
 import os
 
@@ -127,3 +128,13 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Error tags
+
+MESSAGE_TAGS = {
+    constants.DEBUG: 'alert-primary',
+    constants.ERROR: 'alert-danger',
+    constants.SUCCESS: 'alert-success',
+    constants.INFO: 'alert-info',
+    constants.WARNING: 'alert-warning',
+}
