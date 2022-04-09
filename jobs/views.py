62ec1ceaf79bf039228dbfe7342367e0b290825a
Pablo Maribondo <pablomgvieira@gmail.com>
2022-04-08 21:05:04 -0300

    feat: display jobs dynamically

--- jobs/views.py
+++ jobs/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Job
 
 
 def find_jobs(request):
-    return render(request, 'find_jobs.html')
+    if request.method == "GET":
+        jobs = Job.objects.filter(reserved=False)
+        return render(request, 'find_jobs.html', {'jobs': jobs})
