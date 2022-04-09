17ac6e1a50bf15379a0ec934971d5dc8df6ae589
Pablo Maribondo <pablomgvieira@gmail.com>
2022-04-08 21:36:51 -0300

    feat: add jobs filter

--- jobs/views.py
+++ jobs/views.py
@@ -1,8 +1,40 @@
 from django.shortcuts import render
 from .models import Job
+from datetime import datetime
 
 
 def find_jobs(request):
     if request.method == "GET":
-        jobs = Job.objects.filter(reserved=False)
+        min_price = request.GET.get('min_price')
+        max_price = request.GET.get('max_price')
+
+        min_deadline = request.GET.get('min_deadline')
+        max_deadline = request.GET.get('max_deadline')
+
+        category = request.GET.get('category')
+
+        if min_price or max_price or min_deadline or max_deadline or category:
+            if not min_price:
+                min_price = 0
+
+            if not max_price:
+                max_price = 999999
+
+            if not min_deadline:
+                min_deadline = datetime(year=1900, month=1, day=1)
+
+            if not max_deadline:
+                max_deadline = datetime(year=3000, month=1, day=1)
+
+            category = [category, ]
+
+            jobs = Job.objects.filter(price__gte=min_price)\
+                .filter(price__lte=max_price)\
+                .filter(deadline__gte=min_deadline)\
+                .filter(deadline__lte=max_deadline)\
+                .filter(category__in=category)\
+                .filter(reserved=False)
+        else:
+            jobs = Job.objects.filter(reserved=False)
+
         return render(request, 'find_jobs.html', {'jobs': jobs})
